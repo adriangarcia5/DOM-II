92125bafb299001b1157572518c7fa5b2b39b0d5
Adrian-Garcia <agarc5@hotmail.com>
2019-08-06 16:05:48 -0500

    added mouseover/out to index.js & global.less

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+//Mouseover
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', () => {
+    logoHeading.classList.toggle('change-color');
+});
+
+//Mouseout
+logoHeading.addEventListener('mouseout', () => {
+    logoHeading.classList.toggle('change-color-again')
+})
